58d71e13eba0436b5231fb085f21d59974ddb4dc
Imon <contact@imonweb.co.uk>
2022-04-21 00:20:26 +0100

    router

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const cors = require("cors")
+const { errorHandler } = require('../middleware/errorMiddleware')
+ 
 // const { errorHandler } = require('./middleware/errorMiddleware')
 const port = process.env.PORT || 5000
 
 
 const app = express()
-
 app.use(cors())
-app.get('/api/goals', (req, res) => {
-  // res.send('Get goals')
-  // res.json({ message: 'Get goals'})
-  res.status(200).json({ message: 'Get goals'})
-})
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
 app.listen(port, () => console.log(`Server started on port ${port}`))
