5a78f1aeaa85b51dfb3aa3750736d595a10b1812
Imon <contact@imonweb.co.uk>
2022-09-24 23:42:41 +0100

    styles

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,8 @@ const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
 
+
+
 connectDB()
 
 const app = express()
